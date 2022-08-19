5a8ffb8512329def37f73edbb69bda904bf55ef4
Mayya Ə. Soltanova <mayya.soltanova@cybernet.az>
2022-08-19 15:51:04 +0400

    Create sign-up component

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import {createAuthUserWithEmailAndPassword,createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+import './sign-up-form.styles.scss'
+
+const defaultFormFields={
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+}
+const SignUpForm =()=>{
+    const [formFields,setFormFields]=useState(defaultFormFields);
+    const {displayName,email,password,confirmPassword} =formFields;
+    
+    const resetFormFields=()=>{
+        setFormFields(defaultFormFields)
+    }
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target
+        setFormFields({...formFields,[name]:value})
+    }
+    const handleSubmit =async(event)=>{
+        event.preventDefault()
+        if(password!==confirmPassword){
+            return;
+        }
+        try{
+            const {user}=createAuthUserWithEmailAndPassword(email,password)
+            await createUserDocumentFromAuth(user,{displayName})
+            
+            resetFormFields()
+
+        }   catch(error){
+            if(error.code==='auth/email-already-in-use'){
+               alert('cannot create user,email') 
+            }else{            }
+            console.log('error=',error)
+
+        }
+     }
+
+
+    return(
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
+            <form onSubmit={handleSubmit}>
+
+                <FormInput label='Display name' type='text' required onChange={handleChange} name= 'displayName' value={displayName}/>
+
+                <FormInput label='Email' type='email' required onChange={handleChange} name= 'email' value={email}/>
+
+                <FormInput label='Password' type='password' required onChange={handleChange} name= 'password' value={password} />
+
+                <FormInput  label='Confirm Password' type='password' required onChange={handleChange} name= 'confirmPassword' value={confirmPassword}/>
+
+                <Button type='submit'>Sign up</Button>
+
+            </form>
+        </div>
+    )
+}
+export default SignUpForm;
--- src/routes/Sign-in/sign-in.component.jsx
+++ src/routes/Sign-in/sign-in.component.jsx
@@ -1,16 +1,37 @@
-import { signInWithGooglePopup,createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import { auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn=()=>{
-    const logGoogleUser=async()=>
-    {
-        const {user}=await signInWithGooglePopup()
-        const userDocRef=await createUserDocumentFromAuth(user)
-        
+    useEffect(() => {
+        async function fetchMyAPI() {
+            const response =await getRedirectResult(auth);
+            if(response){
+                const userDocRef=await createUserDocumentFromAuth(response)    
+            }
+        }    
+        fetchMyAPI()
+      }, [])
 
-    }
+        const logGoogleUser=async()=>
+        {
+            const {user}=await signInWithGooglePopup()
+            const userDocRef=await createUserDocumentFromAuth(user)
+
+        }
+    
     return (
         <div>
-            <button onClick={logGoogleUser}>Sign in With Google</button>
+            <button onClick={logGoogleUser}>Sign in With Googlee with Popup</button><br/>
+            <button onClick={signInWithGoogleRedirect}>Sign in With Google with Redirect</button>
+            <SignUpForm/>
+
             <h1>
                 Sign In
             </h1>
